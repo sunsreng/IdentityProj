66bf2dd4a945965b4ef47c6a26eff2cfd4dc9aa7
sun sreng <sreng@infinity.com.kh>
2017-08-08 17:22:48 +0700

    ok

--- IdentityProj/Controllers/UserManagementController.cs
+++ IdentityProj/Controllers/UserManagementController.cs
@@ -1,22 +1,47 @@
 using IdentityProj.Models;
 using IdentityProj.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace IdentityProj.Controllers
 {
     public class UserManagementController : Controller
     {
         private readonly IdentityContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserManagementController(IdentityContext dbContext)
+        public UserManagementController(IdentityContext dbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
         public IActionResult Index()
         {
             var vm = new UserManagementIndexViewModel { Users = _dbContext.Users.OrderBy(u => u.Email).ToList() };
             return View(vm);
+        }
+        [HttpGet]
+        public IActionResult AddRole(string id)
+        {
+            var vm = new UserManagementAddRoleViewModel
+            {
+                Roles = _roleManager.Roles.ToList(),
+                UserId=
+            };
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(UserManagementAddRoleViewModel rvm)
+        {
+            var user = await _userManager.FindByIdAsync(rvm.UserId);
+            await _userManager.AddToRoleAsync(user, rvm.NewRole);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ IdentityProj/ViewModels/UserManagementAddRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using IdentityProj.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace IdentityProj.ViewModels
+{
+    public class UserManagementAddRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string NewRole { get; set; }
+        public List<IdentityRole> Roles { get; set; }
+    }
+}
